116d9a424dbb0aaf6484ce84e0348e3676c9f56d
Timothée Rio <rio.timothee@bcg.com>
2023-08-21 17:59:59 +0200

    better recursion stop

--- coloring/homemade_constraint_programing.py
+++ coloring/homemade_constraint_programing.py
@@ -1,10 +1,14 @@
 import numpy as np
 from copy import deepcopy
+import random
+import sys
+
+sys.setrecursionlimit(10000)
 
 # input format node_count, edge_count, edges
 
 
-def build_model_input(node_count, edge_count, edges, initial_number_of_color):
+def build_model_input(node_count, edge_count, edges, initial_number_of_color, strat):
     """Build model main objects."""
 
     # initialiize allowed color for each node (index by node id)
@@ -15,15 +19,24 @@ def build_model_input(node_count, edge_count, edges, initial_number_of_color):
     assigned_nodes = [False for i in range(node_count)]
 
     # neighbor dict
+    global neighbor_list
     neighbor_list = [[] for i in range(node_count)]
     for a, b in edges:
         neighbor_list[b].append(a)
         neighbor_list[a].append(b)
 
+    # assign first node
+    first_node_idx = next(strat(node_domain, assigned_nodes))
+
+    assigned_nodes[first_node_idx] = True
+    node_domain[first_node_idx] = {0}
+    for neighbor in neighbor_list[first_node_idx]:
+        node_domain[neighbor] = node_domain[neighbor] - {0}
+
     return node_domain, neighbor_list, assigned_nodes
 
 
-def propagate_constraint(node_domain, neighbor_list, assigned_nodes):
+def propagate_constraint(node_domain, assigned_nodes):
     has_made_an_update = True
     while has_made_an_update:
         has_made_an_update = False
@@ -38,23 +51,74 @@ def propagate_constraint(node_domain, neighbor_list, assigned_nodes):
 
     return (
         node_domain,
-        neighbor_list,
         assigned_nodes,
     )
 
 
-def is_feasible(node_domain, neighbor_list, assigned_nodes):
+def is_feasible(node_domain, assigned_nodes):
     for node_idx, domain in enumerate(node_domain):
         if len(domain) == 0:
             return False
     return True
 
 
-def choose_next_node(node_domain, neighbor_list, assigned_nodes):
+def choose_next_node_color_min(node_domain, assigned_nodes):
     number_of_neighbor_list = []
     for node_idx in range(len(node_domain)):
         number_of_neighbor_list.append(
-            (node_idx, len(neighbor_list[node_idx]), -len(node_domain[node_idx]))
+            (
+                node_idx,
+                -len(neighbor_list[node_idx]),
+                len(node_domain[node_idx]),
+            )
+        )
+    number_of_neighbor_list.sort(key=lambda a: (a[2], a[1]))
+    for node_idx, _, _ in number_of_neighbor_list:
+        if not assigned_nodes[node_idx]:
+            yield node_idx
+
+
+def choose_next_node_color_max(node_domain, assigned_nodes):
+    number_of_neighbor_list = []
+    for node_idx in range(len(node_domain)):
+        number_of_neighbor_list.append(
+            (
+                node_idx,
+                len(neighbor_list[node_idx]),
+                len(node_domain[node_idx]),
+            )
+        )
+    number_of_neighbor_list.sort(key=lambda a: (a[2], a[1]), reverse=True)
+    for node_idx, _, _ in number_of_neighbor_list:
+        if not assigned_nodes[node_idx]:
+            yield node_idx
+
+
+def choose_next_node_domain_global_min(node_domain, assigned_nodes):
+    number_of_neighbor_list = []
+    for node_idx in range(len(node_domain)):
+        number_of_neighbor_list.append(
+            (
+                node_idx,
+                len(neighbor_list[node_idx]),
+                len(node_domain[node_idx]),
+            )
+        )
+    number_of_neighbor_list.sort(key=lambda a: (a[1], a[2]))
+    for node_idx, _, _ in number_of_neighbor_list:
+        if not assigned_nodes[node_idx]:
+            yield node_idx
+
+
+def choose_next_node_domain_global_max(node_domain, assigned_nodes):
+    number_of_neighbor_list = []
+    for node_idx in range(len(node_domain)):
+        number_of_neighbor_list.append(
+            (
+                node_idx,
+                len(neighbor_list[node_idx]),
+                -len(node_domain[node_idx]),
+            )
         )
     number_of_neighbor_list.sort(key=lambda a: (a[1], a[2]), reverse=True)
     for node_idx, _, _ in number_of_neighbor_list:
@@ -70,21 +134,26 @@ def choose_next_color(node_domain):
         yield color
 
 
-def run_recursion(node_domain, neighbor_list, assigned_nodes):
-    # propagate constraint
-    node_domain, neighbor_list, assigned_nodes = propagate_constraint(
-        node_domain, neighbor_list, assigned_nodes
-    )
+def compute_number_of_color(node_domain, assigned_nodes):
+    color_set = set([])
+    for node_idx, dom in enumerate(node_domain):
+        if assigned_nodes[node_idx]:
+            color_set.update(dom)
+    return len(color_set)
 
-    # check if feasible
-    if not is_feasible(node_domain, neighbor_list, assigned_nodes):
-        return False
+
+def run_recursion(node_domain, assigned_nodes, strat):
+    # propagate constraint
 
     if sum(assigned_nodes) == len(assigned_nodes):
-        return node_domain, neighbor_list, assigned_nodes
+        return node_domain, assigned_nodes
 
-    for node_idx in choose_next_node(node_domain, neighbor_list, assigned_nodes):
-        color_domain = set(node_domain[node_idx])
+    # check if feasible
+    if not is_feasible(node_domain, assigned_nodes):
+        return False
+
+    for node_idx in strat(node_domain, assigned_nodes):
+        color_domain = list(node_domain[node_idx])
         for color in color_domain:
             # copy for recursion
             sub_node_domain = deepcopy(node_domain)
@@ -92,34 +161,55 @@ def run_recursion(node_domain, neighbor_list, assigned_nodes):
 
             sub_node_domain[node_idx] = set([color])
 
-            next_sol = run_recursion(
-                sub_node_domain,
-                neighbor_list,
-                sub_assigned_nodes,
+            sub_node_domain, sub_assigned_nodes = propagate_constraint(
+                sub_node_domain, sub_assigned_nodes
             )
+
+            next_sol = run_recursion(sub_node_domain, sub_assigned_nodes, strat)
+
             if next_sol:
                 return next_sol
+        return False
 
-            # if not feasible we backtrack
-            assigned_nodes[node_idx] = False
-            node_domain[node_idx] = color_domain
+
+def chose_next_node_color(node_domain, assigned_nodes, color):
+    for i, dom in enumerate(node_domain):
+        if color in dom and not assigned_nodes[i]:
+            yield i
 
 
 def run_home_made_solver(node_count, edge_count, edges):
-    for initial_number_of_color in range(4, node_count + 1):
+    min_number_of_color = node_count
+    best_sol = None
+
+    for i, strat in enumerate(
+        [
+            choose_next_node_domain_global_max,
+            choose_next_node_domain_global_min,
+            choose_next_node_color_max,
+            choose_next_node_color_min,
+        ]
+    ):
+        print(f"strategy:{i} tried")
         node_domain, neighbor_list, assigned_nodes = build_model_input(
-            node_count, edge_count, edges, initial_number_of_color
+            node_count, edge_count, edges, min_number_of_color, strat
         )
+        solution = run_recursion(node_domain, assigned_nodes, strat)
 
-        solution = run_recursion(node_domain, neighbor_list, assigned_nodes)
+        node_domain = solution[0]
+        assigned_nodes = solution[1]
 
-        if solution:
-            # a solution has been found
-            node_domain = solution[0]
-            break
+        number_of_color = int(compute_number_of_color(node_domain, assigned_nodes))
 
+        if number_of_color < min_number_of_color:
+            print(f"new_min:{number_of_color}")
+            min_number_of_color = number_of_color
+            best_sol = deepcopy(solution)
     # get node assignement
+    node_domain = best_sol[0]
+    assigned_nodes = best_sol[1]
     node_value = [0] * node_count
     for node_idx, domain in enumerate(node_domain):
         node_value[node_idx] = domain.pop()
-    return int(initial_number_of_color), node_value
+
+    return min_number_of_color, node_value
--- coloring/solver.py
+++ coloring/solver.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from mysolution import Solver
+from mysolutionsolver import Solver
+from homemade_constraint_programing import run_home_made_solver
 
 
 def parser(input_data):
@@ -25,13 +26,17 @@ def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
     node_count, edge_count, edges = parser(input_data)
+    print(f"{node_count} - {edge_count}")
 
     # build a trivial solution
     # every node has its own color
-    solution = Solver(node_count, edge_count, edges).solve()
+    if node_count < 1:
+        opt_sol, solution = Solver(node_count, edge_count, edges).solve()
+    else:
+        opt_sol, solution = run_home_made_solver(node_count, edge_count, edges)
 
     # prepare the solution in the specified output format
-    output_data = str(node_count) + " " + str(0) + "\n"
+    output_data = str(int(opt_sol)) + " " + str(0) + "\n"
     output_data += " ".join(map(str, solution))
 
     return output_data
@@ -42,18 +47,18 @@ import sys
 if __name__ == "__main__":
     import sys
 
-    with open("coloring/data/gc_4_1", "r") as input_data_file:
-        input_data = input_data_file.read()
-    print(solve_it(input_data))
+    # with open("coloring/data/gc_50_1", "r") as input_data_file:
+    #     input_data = input_data_file.read()
+    # print(solve_it(input_data))
 
-    # import sys
+    import sys
 
-    # if len(sys.argv) > 1:
-    #     file_location = sys.argv[1].strip()
-    #     with open(file_location, "r") as input_data_file:
-    #         input_data = input_data_file.read()
-    #     print(solve_it(input_data))
-    # else:
-    #     print(
-    #         "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)"
-    #     )
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
+        with open(file_location, "r") as input_data_file:
+            input_data = input_data_file.read()
+        print(solve_it(input_data))
+    else:
+        print(
+            "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)"
+        )
