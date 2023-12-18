019783832e7cabe1a3a634458f1d6158490698a8
Timothée Rio <rio.timothee@bcg.com>
2023-12-18 14:25:48 +0100

    add tiling

--- facility/or_tools_solver.py
+++ facility/or_tools_solver.py
@@ -15,13 +15,13 @@ def euclidian_distance(x1, y1, x2, y2):
 
 def create_distance_matrix(facilities, customers):
     matrix = np.zeros((len(facilities), len(customers)))
-    for i, facility in enumerate(facilities):
-        for j, customer in enumerate(customers):
-            matrix[i, j] = euclidian_distance(
-                facility.location.x,
-                facility.location.y,
-                customer.location.x,
-                customer.location.y,
+    for f in facilities:
+        for c in customers:
+            matrix[f.index, c.index] = euclidian_distance(
+                f.location.x,
+                f.location.y,
+                c.location.x,
+                c.location.y,
             )
     return matrix
 
@@ -73,7 +73,14 @@ def create_problem_matricies(input_data):
     setup_cost_array = create_setup_cost_array(facilities)
     customer_demand_array = create_customer_demand_array(customers)
     capacity_array = create_capacity_array(facilities)
-    return distance_matrix, setup_cost_array, customer_demand_array, capacity_array, facilities, customers
+    return (
+        distance_matrix,
+        setup_cost_array,
+        customer_demand_array,
+        capacity_array,
+        facilities,
+        customers,
+    )
 
 
 def create_model():
@@ -84,42 +91,51 @@ def create_model():
 def create_variables(model, facilities, customers):
     # create variables
     x = {}
-    for i in range(len(facilities)):
-        for j in range(len(customers)):
+    for f in facilities:
+        for c in customers:
             # x defines whether a customer is assigned to a facility
-            x[i, j] = model.NewBoolVar("x[%i,%i]" % (i, j))
+            x[f.index, c.index] = model.NewBoolVar("x[%i,%i]" % (f.index, c.index))
     y = {}
-    for i in range(len(facilities)):
+    for f in facilities:
         # y defines wether a facility is open
-        y[i] = model.NewBoolVar("y[%i]" % i)
+        y[f.index] = model.NewBoolVar("y[%i]" % f.index)
     return x, y
 
 
-def create_constraints(model, facilities, customers, x,y):
+def create_constraints(model, facilities, customers, x, y):
     # create constraints
-    for j in range(len(customers)):
-        model.Add(sum(x[i, j] for i in range(len(facilities))) == 1)
+    for c in customers:
+        model.Add(sum(x[f.index, c.index] for f in facilities) == 1)
 
-    for i in range(len(facilities)):
-        for j in range(len(customers)):
-            model.Add(x[i, j] <= y[i])
+    for f in facilities:
+        for c in customers:
+            model.Add(x[f.index, c.index] <= y[f.index])
 
-    for i in range(len(facilities)):
+    for f in facilities:
         model.Add(
-            sum(x[i, j] * customers[j].demand for j in range(len(customers)))
-            <= facilities[i].capacity
+            sum(x[f.index, c.index] * c.demand for c in customers)
+            <= f.capacity
         )
     return model
 
 
-def create_objective(model, customers, facilities, distance_matrix, setup_cost_array, customer_demand_array,x,y):
+def create_objective(
+    model,
+    facilities,
+    customers,
+    distance_matrix,
+    setup_cost_array,
+    customer_demand_array,
+    x,
+    y,
+):
     # create objective
     model.Minimize(
         sum(
-            sum(x[i, j] * distance_matrix[i, j] for i in range(len(facilities)))
-            for j in range(len(customers))
+            sum(x[f.index, c.index] * distance_matrix[f.index, c.index] for f in facilities)
+            for c in customers
         )
-        + sum(y[i] * setup_cost_array[i] for i in range(len(facilities)))
+        + sum(y[f.index] * f.setup_cost for f in facilities)
     )
     return model
 
@@ -132,38 +148,89 @@ def solve_model(model):
     return solver, status
 
 
-def main_solver(input_data):
+def main_solver(input_data, number_of_tiles_per_border = 4):
     (
         distance_matrix,
         setup_cost_array,
         customer_demand_array,
         capacity_array,
-        facilities, 
-        customers
+        facilities,
+        customers,
     ) = create_problem_matricies(input_data)
-    model = create_model()
-    x, y = create_variables(model, facilities, customers)
-    model = create_constraints(model, facilities, customers,x,y)
-    model = create_objective(
-        model, customers, facilities, distance_matrix, setup_cost_array, customer_demand_array,x,y
+
+    already_assigned_facilities = [False for i in range(len(facilities))]
+    already_assigned_customers = [False for i in range(len(customers))]
+
+    # find x min of facility and customer
+    assets = facilities + customers
+    x_min, x_max = min([asset.location.x for asset in assets]), max(
+        [asset.location.x for asset in assets]
     )
-    solver, status = solve_model(model)
+    y_min, y_max = min([asset.location.y for asset in assets]), max(
+        [asset.location.y for asset in assets]
+    )
 
-    if status == cp_model.OPTIMAL:
+    x_tile_lenght = (x_max - x_min) / number_of_tiles_per_border
+    y_tile_lenght = (y_max - y_min) / number_of_tiles_per_border
 
-        optimal_value = solver.ObjectiveValue()
-        assignements = []
-        for j in range(len(customers)):
-            value = solver.Value(
-                        np.argmax(
-                            [solver.Value(x[i, j]) for i in range(len(facilities))]
-                        )
-                    )
-            assignements.append(value)
+    optimal_value = 0
+    assignements = [-1 for i in range(len(customers))]
 
-        output_data = '%.2f' % optimal_value + ' ' + str(0) + '\n'
-        output_data += ' '.join(map(str, assignements))
-        return output_data
-    else:
-        print("No optimal solution found!")
-        return None
+    for i in range(number_of_tiles_per_border):
+        for j in range(number_of_tiles_per_border):
+            tile_id = i * number_of_tiles_per_border + j
+            print(f"tile {tile_id} of {number_of_tiles_per_border**2}")
+            tile_x_min = x_min + i * x_tile_lenght
+            tile_x_max = x_min + (i + 1) * x_tile_lenght
+            tile_y_min = y_min + j * y_tile_lenght
+            tile_y_max = y_min + (j + 1) * y_tile_lenght
+
+            # create subproblem         
+            sub_facilities = [
+                facility
+                for facility in facilities
+                if tile_x_min <= facility.location.x <= tile_x_max
+                and tile_y_min <= facility.location.y <= tile_y_max
+                and not already_assigned_facilities[facility.index]
+            ]
+            sub_customers = [
+                customer
+                for customer in customers
+                if tile_x_min <= customer.location.x <= tile_x_max
+                and tile_y_min <= customer.location.y <= tile_y_max
+                and not already_assigned_customers[customer.index]
+            ]
+            for customer in sub_customers:
+                already_assigned_customers[customer.index] = True
+            for facility in sub_facilities:
+                already_assigned_facilities[facility.index] = True
+
+            model = create_model()
+            x, y = create_variables(model, sub_facilities, sub_customers)
+   
+            model = create_constraints(model, sub_facilities, sub_customers, x, y)
+            model = create_objective(
+                model,
+                sub_facilities,
+                sub_customers,
+                distance_matrix,
+                setup_cost_array,
+                customer_demand_array,
+                x,
+                y,
+            )
+            solver, status = solve_model(model)
+
+            if status == cp_model.OPTIMAL:
+                optimal_value += solver.ObjectiveValue()
+                for c in sub_customers:
+                    for f in sub_facilities:
+                        if solver.Value(x[f.index, c.index]) == 1:
+                            assignements[c.index] = f.index
+                            break
+            else:
+                print("No optimal solution found!")
+                return None
+    output_data = "%.2f" % optimal_value + " " + str(0) + "\n"
+    output_data += " ".join(map(str, assignements))
+    return output_data
